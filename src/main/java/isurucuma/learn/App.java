6484aebb31b2105c469afc2e7827888c722ce8f9
isuru <isurucuma@gmail.com>
2025-01-18 01:06:39 +0530

    initial commit

---
+++ src/main/java/isurucuma/learn/App.java
@@ -0,0 +1,13 @@
+package isurucuma.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
